42ba43bb865b84a1de3b0e7dd02304115cf4b742
Alexander01998 <contact.wurstimperium@gmail.com>
2018-08-13 20:54:48 +0200

    Add range setting & debug draw to AutoFish

--- ForgeWurst MC 1.12.2/src/shared/java/net/wurstclient/forge/hacks/AutoFishHack.java
+++ ForgeWurst MC 1.12.2/src/shared/java/net/wurstclient/forge/hacks/AutoFishHack.java
@@ -9,14 +9,21 @@ package net.wurstclient.forge.hacks;
 
 import java.lang.reflect.Method;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.entity.projectile.EntityFishHook;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemFishingRod;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.SPacketSoundEffect;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.wurstclient.fmlevents.WPacketInputEvent;
@@ -26,12 +33,32 @@ import net.wurstclient.forge.Hack;
 import net.wurstclient.forge.compatibility.WEnchantments;
 import net.wurstclient.forge.compatibility.WItem;
 import net.wurstclient.forge.compatibility.WMinecraft;
+import net.wurstclient.forge.compatibility.WVec3d;
+import net.wurstclient.forge.settings.CheckboxSetting;
+import net.wurstclient.forge.settings.SliderSetting;
+import net.wurstclient.forge.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.forge.utils.ChatUtils;
 import net.wurstclient.forge.utils.PlayerControllerUtils;
+import net.wurstclient.forge.utils.RenderUtils;
 
 public final class AutoFishHack extends Hack
 {
+	private final SliderSetting validRange = new SliderSetting("Valid range",
+		"Any bites that occur outside of this range\n" + "will be ignored.\n\n"
+			+ "Increase your range if bites are not being\n"
+			+ "detected, decrease it if other people's\n"
+			+ "bites are being detected as yours.",
+		1.5, 0.25, 8, 0.25, ValueDisplay.DECIMAL);
+	private CheckboxSetting debugDraw = new CheckboxSetting("Debug draw",
+		"Shows where bites are occurring and where\n"
+			+ "they will be detected. Useful for optimizing\n"
+			+ "your 'Valid range' setting.",
+		false);
+	
 	private int timer;
+	private Vec3d lastSoundPos;
+	private int box;
+	private int cross;
 	
 	public AutoFishHack()
 	{
@@ -39,12 +66,29 @@ public final class AutoFishHack extends Hack
 			+ "best fishing rod. If it finds a better\n"
 			+ "rod while fishing, it will automatically\n" + "switch to it.");
 		setCategory(Category.OTHER);
+		addSetting(validRange);
+		addSetting(debugDraw);
 	}
 	
 	@Override
 	protected void onEnable()
 	{
 		timer = 0;
+		lastSoundPos = null;
+		
+		box = GL11.glGenLists(1);
+		
+		cross = GL11.glGenLists(1);
+		GL11.glNewList(cross, GL11.GL_COMPILE);
+		GL11.glColor4f(1, 0, 0, 0.5F);
+		GL11.glBegin(GL11.GL_LINES);
+		GL11.glVertex3d(-0.125, 0, -0.125);
+		GL11.glVertex3d(0.125, 0, 0.125);
+		GL11.glVertex3d(0.125, 0, -0.125);
+		GL11.glVertex3d(-0.125, 0, 0.125);
+		GL11.glEnd();
+		GL11.glEndList();
+		
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
@@ -52,11 +96,27 @@ public final class AutoFishHack extends Hack
 	protected void onDisable()
 	{
 		MinecraftForge.EVENT_BUS.unregister(this);
+		GL11.glDeleteLists(box, 1);
+		GL11.glDeleteLists(cross, 1);
 	}
 	
 	@SubscribeEvent
 	public void onUpdate(WUpdateEvent event)
 	{
+		// update range box
+		if(debugDraw.isChecked())
+		{
+			GL11.glNewList(box, GL11.GL_COMPILE);
+			AxisAlignedBB box = new AxisAlignedBB(-validRange.getValue(),
+				-1 / 16.0, -validRange.getValue(), validRange.getValue(),
+				1 / 16.0, validRange.getValue());
+			GL11.glColor4f(1, 0, 0, 0.5F);
+			GL11.glBegin(GL11.GL_LINES);
+			RenderUtils.drawOutlinedBox(box);
+			GL11.glEnd();
+			GL11.glEndList();
+		}
+		
 		EntityPlayerSP player = event.getPlayer();
 		InventoryPlayer inventory = player.inventory;
 		
@@ -135,17 +195,77 @@ public final class AutoFishHack extends Hack
 	@SubscribeEvent
 	public void onPacketInput(WPacketInputEvent event)
 	{
-		// check packet type
+		EntityPlayerSP player = WMinecraft.getPlayer();
+		if(player == null || player.fishEntity == null)
+			return;
+		
 		if(!(event.getPacket() instanceof SPacketSoundEffect))
 			return;
 		
 		// check sound type
-		if(!SoundEvents.ENTITY_BOBBER_SPLASH
-			.equals(((SPacketSoundEffect)event.getPacket()).getSound()))
+		SPacketSoundEffect sound = (SPacketSoundEffect)event.getPacket();
+		if(!SoundEvents.ENTITY_BOBBER_SPLASH.equals(sound.getSound()))
+			return;
+		
+		if(debugDraw.isChecked())
+			lastSoundPos = new Vec3d(sound.getX(), sound.getY(), sound.getZ());
+		
+		// check position
+		EntityFishHook bobber = player.fishEntity;
+		if(Math.abs(sound.getX() - bobber.posX) > validRange.getValue()
+			|| Math.abs(sound.getZ() - bobber.posZ) > validRange.getValue())
 			return;
 		
 		// catch fish
 		rightClick();
+	}
+	
+	@SubscribeEvent
+	public void onRenderWorldLast(RenderWorldLastEvent event)
+	{
+		if(!debugDraw.isChecked())
+			return;
+		
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-TileEntityRendererDispatcher.staticPlayerX,
+			-TileEntityRendererDispatcher.staticPlayerY,
+			-TileEntityRendererDispatcher.staticPlayerZ);
+		
+		EntityFishHook bobber = WMinecraft.getPlayer().fishEntity;
+		if(bobber != null)
+		{
+			GL11.glPushMatrix();
+			GL11.glTranslated(bobber.posX, bobber.posY, bobber.posZ);
+			GL11.glCallList(box);
+			GL11.glPopMatrix();
+		}
+		
+		if(lastSoundPos != null)
+		{
+			GL11.glPushMatrix();
+			GL11.glTranslated(WVec3d.getX(lastSoundPos),
+				WVec3d.getY(lastSoundPos), WVec3d.getZ(lastSoundPos));
+			GL11.glCallList(cross);
+			GL11.glPopMatrix();
+		}
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glColor4f(1, 1, 1, 1);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 	
 	private int getRodValue(ItemStack stack)
